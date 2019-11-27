fe47603eb03724ec019e85f4b6c100d8bcecda07
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-27 14:29:19 +0900

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/edit'
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
