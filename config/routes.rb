5d1a716da504e3e74bd20e524dede9f011267140
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-28 15:21:24 +0900

    make top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/edit'
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments, only: :create
+  end
 end
