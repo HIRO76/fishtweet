dd86a0604b9375148845639f70a6317b6bd9ac6d
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-28 15:45:57 +0900

    first commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit]
+
   def index
+    # @articles = Article.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+  end
+
+  private 
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
