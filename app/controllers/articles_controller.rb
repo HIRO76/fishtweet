53b984f856dbfb011c636019a1118bbf1cebf96c
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-11-28 20:38:13 +0900

    create articles show_page & form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit]
+  # before_action :set_article, only: [:show, :edit]
 
   def index
-    # @articles = Article.new
+    # @articles = Article.all
   end
 
   def show
@@ -11,12 +11,22 @@ class ArticlesController < ApplicationController
   def edit
   end
 
+  def new
+    # @article = Article.new
+  end
+
   def create
+    Article.create(title: article_params[:title], description: article_params[:description], body: article_params[:body], images: article_params[:images])
+    redirect_to @article
   end
 
   private 
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, :body, images: [])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
-  has_many :images, dependent: :destroy_all
+  has_many_attached :images, dependent: :destroy_all
   has_many :comments, dependent: :destroy_all
   belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, presence: true
 end
