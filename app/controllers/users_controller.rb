d67b7c62122828a9f34aa8a4ffb8d62446b0ebdb
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-12-04 21:11:54 +0900

    make user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @articles = Article.where(user_id: current_user.id).order(created_at: :desc).limit(10)
   end
 
   def edit
-  end
-
-  def create
   end
 end
