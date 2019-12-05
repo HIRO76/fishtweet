db50f6ed349216acb4929d3ea79e5d34d700d1ec
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-12-05 19:24:38 +0900

    maked article destroy function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    article = Article.find(params[:id])
+    if article.user_id === current_user.id
+      article.destroy
+      redirect_to controller: "users", action: "show"
+    end
   end
 
 
