2111c0b32754aa9319d2f88890cde90c9137b96d
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-12-06 12:54:08 +0900

    make user edit function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_signup_parameters, if: :devise_controller?
+  before_action :configure_update_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  def configure_signup_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avatar])
+  end
+
+  def configure_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :avatar])
   end
   
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,13 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit]
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_name, only: [:index, :show, :edit]
+  # before_action :set_name, only: [:index, :show, :edit]
 
   helper_method :images_destroy
 
 
   def index
-    @articles = Article.limit(10).order(created_at: :desc)
+    @articles = Article.includes(:user).limit(10).order(created_at: :desc)
   end
 
   def show
@@ -68,9 +68,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def set_name
-    @nickname = current_user.nickname
-  end
+  # def set_name
+  #   # @article =  Article.find(params[:id])
+  #   @nickname = User.find(@article.user_id)
+  # end
 
   def article_params
     params.require(:article).permit(:title, :description, :body, images: []).merge(user_id: current_user.id)
