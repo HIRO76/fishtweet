dd93a22d55ba796e4a3171ae6b2534f25be00d36
hiroyuki ishizuka <ikushin0706@gmail.com>
2019-12-08 17:33:41 +0900

    added featurespec

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -4,9 +4,9 @@ feature 'article', type: :feature do
   let(:user) { create(:user) }
   scenario '記事投稿' do 
     # signin前
-    # visit root_path
-    # click_on('投稿する')
-    # expect(page).to eq root_path
+    visit root_path
+    click_on('投稿する')
+    expect(page).to eq new_article_path
 
     # signin
     visit new_user_session_path
@@ -14,7 +14,7 @@ feature 'article', type: :feature do
     fill_in 'user_password', with: user.password
     find('input[type="submit"]').click
     expect(current_path).to eq root_path
-    expect(page).to have_content('投稿する')
+    expect(page).to have_content('ログインしました')
 
     # post
     expect {
@@ -24,5 +24,20 @@ feature 'article', type: :feature do
       fill_in 'article[body]',  with: 'Rspec フューチャスペックのテスト'
       find('input[type="submit"]').click
     }.to change(Article, :count).by(1)
+  end
+
+  scenario '記事投稿失敗' do 
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('input[type="submit"]').click
+
+    visit root_path
+    visit new_article_path
+    fill_in 'article[title]', with: ""
+    fill_in 'article[body]', with: ""
+    find('input[type="submit"]').click
+    expect(article.errors[:title]).to heve_content("記事を入力してください")
+    expect(article.errors[:body]).to heve_content("記事を入力してください")
   end
 end
